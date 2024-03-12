5e5fb90b949734b96224fe13ea4d4396f7238bb9
kimikanna <svartirose@gmail.com>
2024-03-12 07:06:48 +0500

    solved part 1 tasks 1-3

--- part_1/task_1.py
+++ part_1/task_1.py
@@ -1,6 +1,8 @@
-def getPerimeter(side):
+# Дана сторона квадрата a. Найти его периметр P = 4·a
+
+def get_perimeter(side):
     return(4 * side)
 
 
-side = int(input("Введите длину стороны квадрата: "))
-print("Периметр:", getPerimeter(side))
+side = float(input("Введите длину стороны квадрата: "))
+print("Периметр:", get_perimeter(side))
